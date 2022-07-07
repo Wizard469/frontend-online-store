9b16dca1645e6d8e5a0f623a4e716327a965693a
Joao Espanha <jpbesr@gmail.com>
2022-07-07 16:28:49 -0300

    implementando req 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
+import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
+
 import './App.css';
 // comecando mais
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [],
+
+    };
+  }
+
+  componentDidMount() {
+    this.setCategories();
+    this.handleSearch();
+  }
+
+  setCategories = async () => {
+    const categories = await getCategories();
+
+    this.setState({
+      categories,
+    });
+  }
+
+  handleSearch = async () => {
+    const result = await getProductsFromCategoryAndQuery('', 'computador');
+    return result;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={ logo } className="App-logo" alt="logo" />
+          <p>Edit src/App.js and save to reload.</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
