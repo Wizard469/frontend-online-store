639d14b5fe1ce09129c9290fffba5d8a4106de73
Henrique Rosa <samuel_kuik@hotmail.com>
2022-07-08 14:14:40 -0300

    Corrigindo Lint

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import CardProduct from './components/CardProduct';
 import { Link } from 'react-router-dom';
+import CardProduct from './components/CardProduct';
 import CategoriesMenu from '../components/CategoriesMenu';
 
 class Home extends React.Component {
